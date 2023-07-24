46d6d8a111c6c50d4f142bcf82289dbb33629d16
sigurdurhaukur <56935611+sigurdurhaukur@users.noreply.github.com>
2023-07-24 23:27:21 +0000

    increased performance

--- object-detection/main.py
+++ object-detection/main.py
@@ -1,19 +1,28 @@
 import cv2
 from ultralytics import YOLO
 
-# Load the model
-
-cap = cv2.VideoCapture("./road-traffic.mp4")
-
-model = YOLO("yolov8m.pt")
+cap = cv2.VideoCapture("./iceland-traffic-cleaned.mp4")  # load the video
+model = YOLO("yolov8m.pt")  # lode the model
 
 
 def predict(frame):
-    results = model(frame)
+    """
+    Predicts the bounding boxes, classes, and confidences of the objects in the bottom half of the frame (for performance).
+    """
+    frame_height, frame_width, _ = frame.shape
+
+    # Crop the frame to analyze only the bottom half
+    half_height = frame_height // 2
+    bottom_half_frame = frame[half_height:, :, :]
+
+    results = model(bottom_half_frame)
     results = results[0]
     bboxes = results.boxes.xyxy.cpu().numpy().astype(int)
     classes = results.boxes.cls.cpu().numpy().astype(int)
-    confidences = results.boxes.conf.cpu().numpy()
+    confidences = results.boxes.conf.cpu().numpy().astype(float)
+
+    # Shift the bounding boxes' y-coordinates to match the original frame
+    bboxes[:, [1, 3]] += half_height
 
     return bboxes, classes, confidences
 
@@ -28,16 +37,21 @@ while True:
         break
 
     bboxes, classes, confidences = predict(frame)
+    # total number of objects detected
+
+    amount_of_each_class = {}
 
     for bbox, cls, confidence in zip(bboxes, classes, confidences):
         x1, y1, x2, y2 = bbox
-        class_name = model.names[cls]
+        class_name = model.names[cls]  # type of object detected
 
         # only show if confidence is greater than 0.5
         if confidence < 0.5:
             continue
 
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
+        cv2.rectangle(
+            frame, (x1, y1), (x2, y2), (0, 255, 0), 1
+        )  # draw the bounding box
         cv2.putText(
             frame,
             class_name,
@@ -46,6 +60,24 @@ while True:
             0.5,
             (0, 255, 0),
             1,
+        )
+
+        if class_name not in amount_of_each_class:
+            amount_of_each_class[class_name] = 1
+        else:
+            amount_of_each_class[class_name] += 1
+
+    # display the number of objects detected
+    for i, (class_name, amount) in enumerate(amount_of_each_class.items()):
+        distance_from_top = 40 * (i + 1)
+        cv2.putText(
+            frame,
+            f"{class_name}: {amount}",
+            (10, 30 + distance_from_top),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 0, 0),
+            2,
         )
 
     cv2.imshow("frame", frame)
